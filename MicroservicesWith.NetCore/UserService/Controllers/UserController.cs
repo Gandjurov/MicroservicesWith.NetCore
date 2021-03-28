4c554e3624c22e8f2fb38046cee55e2f91a34b50
HGandjurov <gandjurov@abv.bg>
2021-03-29 00:11:42 +0300

    Add Database and CRUD operations

--- MicroservicesWith.NetCore/UserService/Controllers/UserController.cs
+++ MicroservicesWith.NetCore/UserService/Controllers/UserController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserService.Database;
+using UserService.Database.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,36 +13,66 @@ namespace UserService.Controllers
 {
     public class UserController : ApiController
     {
+        private UsersDbContext context;
+        public UserController(UsersDbContext context)
+        {
+            this.context = context;
+        }
         // GET: api/<UserController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<UserModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            return context.Users.ToList();
         }
 
         // GET api/<UserController>/5
-        [HttpGet(Id)]
-        public string Get(int id)
+        [HttpGet(Id, Name = "Get")]
+        public UserModel Get(int id)
         {
-            return "value";
+            return context.Users.Find(id);
         }
 
         // POST api/<UserController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] UserModel user)
         {
+            if (!ModelState.IsValid)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created, user);
         }
 
         // PUT api/<UserController>/5
         [HttpPut(Id)]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] UserModel user)
         {
+            if (!ModelState.IsValid)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            UserModel currentUser = context.Users.Find(user.UserId);
+            currentUser.Name = user.Name;
+
+            context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created, user);
         }
 
         // DELETE api/<UserController>/5
         [HttpDelete(Id)]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            UserModel user = context.Users.Find(id);
+
+            if (user == null)
+                return NotFound();
+
+            context.Users.Remove(user);
+            context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
--- MicroservicesWith.NetCore/UserService/Startup.cs
+++ MicroservicesWith.NetCore/UserService/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserService.Database;
 
 namespace UserService
 {
@@ -26,6 +28,7 @@ namespace UserService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<UsersDbContext>(Options => Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
