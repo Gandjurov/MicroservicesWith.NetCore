ac30815daa05f2bc41227f3aa7689755959aaba3
HGandjurov <gandjurov@abv.bg>
2021-03-28 23:40:12 +0300

    Add Simple UserService

---
+++ MicroservicesWith.NetCore/UserService/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserService.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet(Id)]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut(Id)]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete(Id)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
